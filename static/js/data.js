72ccf54f31e0f8dcc0ba7876f848ed142f8da866
jiajia <3212946755@qq.com>
2017-04-16 23:44:23 +0800

    增加相册和文章功能

---
+++ static/js/data.js
@@ -0,0 +1,25 @@
+var photosData = [
+  {
+    srcData:'',
+    altData:'',
+    topData:'',
+    bottomData:''
+  },
+  {
+    srcData:'',
+    altData:''
+    topData:'',
+    bottomData:''
+  }
+]
+
+var articlesData = [
+  {
+    contentData:'',
+    titleData:''
+  },
+  {
+    contentData:'',
+    titleData:''
+  }
+]

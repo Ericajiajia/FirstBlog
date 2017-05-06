e3e3bb6c157c50a6dd52df0236256c16591b9bd3
jiajia <3212946755@qq.com>
2017-05-06 23:53:01 +0800

    调整代码结构

--- static/js/article.js
+++ static/js/article.js
@@ -1,20 +1,35 @@
-var labeling = $('.guide-summary label');
 var parent = $('.guide-summary')[0]
+
+function turnArray (num, labeling, uling) {
+  if(labeling[num].className.indexOf("0") !== -1) {
+    labeling[num].className = "guide-label1"
+    uling[num].style.display = "none"
+  } else if (labeling[num].className.indexOf("1") !== -1) {
+    labeling[num].className = "guide-label0"
+    uling[num].style.display = "block"
+  }
+}
 
 // 给分类绑定事件委托
 parent.addEventListener('click', function (e) {
   var cateName
+  var labeling = $(".guide-summary label")
+  var uling = $(".guide-summary ul")
 
-  if (e.target.className === 'category-title') {
-    cateName = e.target.dataset.name
+  if (e.target.nodeName === 'P') {
+    cateName = e.target.parentNode.dataset.name
     switch (cateName) {
       case 'fe':
+        turnArray (0, labeling, uling)
+        break;
       case 'alog':
-        break
+        turnArray (1, labeling, uling)
+        break;
       case 'note':
-        break
+        turnArray (2, labeling, uling)
+        break;
       default:
-        break
+        break;
     }
   }
 })
